e9e593d230fd45805b3ddb098d173b3b7d2b0650
alumi <alumi@users.noreply.github.com>
2021-02-02 09:46:58 +0900

    Tweak :exclusions of :dependencies

--- project.clj
+++ project.clj
@@ -8,24 +8,20 @@
                  [org.clojure/core.async "1.3.610"]
                  [integrant "0.8.0"]
                  [duct/core "0.8.0"
-                  :exclusions [integrant]]
+                  :exclusions [medley]]
                  [duct/module.logging "0.5.0"]
                  [duct/module.sql "0.6.1"]
                  [duct/module.web "0.7.1"
-                  :exclusions [metosin/muuntaja
-                               ring/ring-codec
-                               org.eclipse.jetty/jetty-server]]
+                  :exclusions [ring/ring-core
+                               metosin/jsonista
+                               com.fasterxml.jackson.datatype/jackson-datatype-jsr310]]
                  [org.eclipse.jetty/jetty-server "9.4.36.v20210114"]
-                 [ring/ring-codec "1.1.2"
-                  :exclusions [commons-codec]] ;; duct/module.web
-                 [commons-codec "1.15"] ;; ring/ring-codec
                  [org.apache.commons/commons-compress "1.20"]
                  [mysql/mysql-connector-java "8.0.23"]
                  [com.layerware/hugsql "0.5.1"]
-                 [metosin/muuntaja "0.6.7"] ;; duct/module.web
-                 [metosin/jsonista "0.3.1"] ;; duct/module.web
                  [metosin/reitit "0.5.11"]
-                 [metosin/ring-http-response "0.9.1"]
+                 [metosin/ring-http-response "0.9.1"
+                  :exclusions [ring/ring-core]]
                  [com.github.docker-java/docker-java "3.2.7"]
                  [javax.activation/activation "1.1.1"]
                  [com.cognitect/anomalies "0.1.12"]
@@ -61,9 +57,11 @@
                   :resource-paths ["dev/resources" "test/resources"]
                   :dependencies   [[integrant/repl "0.3.2"]
                                    [hawk "0.2.11"]
-                                   [eftest "0.5.9"]
+                                   [eftest "0.5.9"
+                                    :exclusions [mvxcvi/puget]]
                                    [kerodon "0.9.1"
-                                    :exclusions [ring/ring-codec]]
+                                    :exclusions [clj-time
+                                                 ring/ring-codec]]
                                    [com.h2database/h2 "1.4.200"]
                                    [com.gearswithingears/shrubbery "0.4.1"]]
                   :global-vars {*warn-on-reflection* true}}})
