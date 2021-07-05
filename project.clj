e487dc4ef293ca8f09d5e987ffb7d2db65cd73cd
alumi <alumi@users.noreply.github.com>
2021-07-05 18:04:39 +0900

    Update io.dropwizard.metrics/metrics-healthchecks to 4.2.2

--- project.clj
+++ project.clj
@@ -33,7 +33,7 @@
                  [instaparse "1.4.10"]
                  [org.flatland/ordered "1.5.9"]
                  [io.dropwizard.metrics/metrics-core "4.2.2"]
-                 [io.dropwizard.metrics/metrics-healthchecks "4.1.17"]]
+                 [io.dropwizard.metrics/metrics-healthchecks "4.2.2"]]
   :plugins [[duct/lein-duct "0.12.1"]
             [lein-eftest "0.5.9"]]
   :main ^:skip-aot genomon-api.main
