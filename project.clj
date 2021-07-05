ab8efd77a9385b6b5300e87e67e3bf193951c478
alumi <alumi@users.noreply.github.com>
2021-07-05 11:59:31 +0900

    Update org.clojure/core.async to 1.3.618

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/core.async "1.3.610"]
+                 [org.clojure/core.async "1.3.618"]
                  [integrant "0.8.0"]
                  [duct/core "0.8.0"
                   :exclusions [medley]]
