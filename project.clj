c4ffdcc319c66ed9f56b590e2b29eb9b251f6044
alumi <alumi@users.noreply.github.com>
2021-10-20 13:32:50 +0900

    Update com.cognitect.aws/api to 0.8.524

--- project.clj
+++ project.clj
@@ -25,7 +25,7 @@
                  [com.github.docker-java/docker-java "3.2.12"]
                  [javax.activation/activation "1.1.1"]
                  [com.cognitect/anomalies "0.1.12"]
-                 [com.cognitect.aws/api "0.8.515"]
+                 [com.cognitect.aws/api "0.8.524"]
                  [com.cognitect.aws/endpoints "1.1.12.13"]
                  [com.cognitect.aws/s3 "811.2.934.0"]
                  [clj-commons/clj-yaml "0.7.106"]
