5f1d6f58815eb4522989f2ddf951e22774ae8e79
Shogo Ohta <shogo@xcoo.jp>
2022-01-20 14:19:52 +0900

    Update com.h2database/h2 to 2.1.210

--- project.clj
+++ project.clj
@@ -67,6 +67,6 @@
                                    [kerodon "0.9.1"
                                     :exclusions [clj-time
                                                  ring/ring-codec]]
-                                   [com.h2database/h2 "2.0.202"]
+                                   [com.h2database/h2 "2.1.210"]
                                    [com.gearswithingears/shrubbery "0.4.1"]]
                   :global-vars {*warn-on-reflection* true}}})
