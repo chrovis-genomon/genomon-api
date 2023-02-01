0b594bc13af50e15d98e4b992237c108202255cd
Shogo Ohta <shogo@xcoo.jp>
2023-02-01 19:40:57 +0900

    Update org.clojure/core.async to 1.6.673

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/core.async "1.5.648"]
+                 [org.clojure/core.async "1.6.673"]
                  [integrant "0.8.0"]
                  [duct/core "0.8.0"
                   :exclusions [medley]]
