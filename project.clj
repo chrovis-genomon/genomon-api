b048021af9d10bdb22636bac226902aad7803686
Shogo Ohta <shogo@xcoo.jp>
2023-02-01 19:32:49 +0900

    Update io.dropwizard.metrics/metrics-healthchecks to 4.2.15

--- project.clj
+++ project.clj
@@ -37,7 +37,7 @@
                  [instaparse "1.4.12"]
                  [org.flatland/ordered "1.15.10"]
                  [io.dropwizard.metrics/metrics-core "4.2.15"]
-                 [io.dropwizard.metrics/metrics-healthchecks "4.2.7"]]
+                 [io.dropwizard.metrics/metrics-healthchecks "4.2.15"]]
   :plugins [[duct/lein-duct "0.12.1"]
             [lein-eftest "0.5.9"]]
   :main ^:skip-aot genomon-api.main
