ebb1af7cbf91b17610891366fbf17d1507a9c555
Shogo Ohta <shogo@xcoo.jp>
2023-02-01 19:36:09 +0900

    Update org.clojure/clojure to 1.11.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "GNU General Pulibc License, Version 3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.async "1.5.648"]
                  [integrant "0.8.0"]
                  [duct/core "0.8.0"
