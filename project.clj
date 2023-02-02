5f817a93ef4eb83444f3406a4228daae98536b26
Shogo Ohta <shogo@xcoo.jp>
2023-02-02 18:28:00 +0900

    Update com.cognitect.aws/s3 to 825.2.1250.0

--- project.clj
+++ project.clj
@@ -32,7 +32,7 @@
                  [com.cognitect/anomalies "0.1.12"]
                  [com.cognitect.aws/api "0.8.641"]
                  [com.cognitect.aws/endpoints "1.1.12.398"]
-                 [com.cognitect.aws/s3 "814.2.1053.0"]
+                 [com.cognitect.aws/s3 "825.2.1250.0"]
                  [camel-snake-kebab "0.4.3"]
                  [instaparse "1.4.12"]
                  [org.flatland/ordered "1.15.10"]
